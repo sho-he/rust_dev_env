15b41e185cb124cafd03b2c0784898b20e9170af
sho-he <s.n.0704.g@gmail.com>
2024-01-15 21:51:37 +0900

    add: Rust execution enviromnent

---
+++ project/hello.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+    // コンパイルされたバイナリが実行されるとこの関数が呼び出されます
+
+    // Print text to the console.
+    // コンソールに文字列を出力する
+    println!("Hello World!");
+}
